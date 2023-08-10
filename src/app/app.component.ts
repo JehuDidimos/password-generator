3c83bf835d3a88e90d7aa4ac7a6c4a55df89918e
JehuDidimos <jehudidimos.cs@gmail.com>
2023-08-10 00:26:00 -0500

    Accept Length, and certain characters and generate password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+
+  protected useLetters:boolean;
+  protected useNumbers:boolean;
+  protected useSymbols:boolean;
+  protected title:string;
+  protected newPassword:string; 
+  protected passwordLength: number
+
+
+  constructor(){
+    this.title = "password-generator";
+    this.newPassword = '';
+    this.useLetters = false;
+    this.useNumbers = false;
+    this.useSymbols = false;
+    this.passwordLength = 0;
+  }
+
+  changeLetterUse(){
+    this.useLetters = !this.useLetters;
+  }
+
+  changeNumberUse(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  changeSymbolUse(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  setPasswordLength(length: any){
+    const parsedvalue = parseInt(length.value);
+    if(!isNaN(parsedvalue)){
+      this.passwordLength = parsedvalue;
+    }
+    console.log(this.passwordLength);
+  }
 
   clicked(){
-    console.log("Button Click");
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%*'
+
+    var validChars = '';
+
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.passwordLength; i++){
+      var index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.newPassword = generatedPassword;
   }
 }
